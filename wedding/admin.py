8a58f6c12ecea4f1b4815f82f832dec913a181d3
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-06-03 23:30:26 +0100

    feat: add models to admin

--- wedding/admin.py
+++ wedding/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from wedding.models import User, Allergy, Rsvp
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Allergy)
+admin.site.register(Rsvp)
