6e79c076cb028b4f296305dd7f0e31fc1c5bbbf0
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-06-20 07:51:39 +0100

    feat: integrate form into new layout

--- wedding/forms.py
+++ wedding/forms.py
@@ -6,23 +6,22 @@ from wedding.models import Allergy
 class RsvpForm(forms.Form):
 
     # this is disgusting, but django made me do it :-(
-    css_class = "shadow appearance-none border border-black rounded " \
-                "py-1 px-1 text-black text-left"
+    css_class = "form-control form-opacity"
 
     num_guests = forms.IntegerField(
         label="Number of guests",
-        widget=forms.NumberInput(attrs={'class': css_class + " w-full"})
+        widget=forms.NumberInput(attrs={'class': css_class})
     )
     allergies = forms.MultipleChoiceField(
         choices=Allergy.ALLERGY_CHOICES,
         label="Allergies",
-        widget=forms.CheckboxSelectMultiple(attrs={'class': css_class}),
+        widget=forms.CheckboxSelectMultiple(attrs={'class': ""}),
         required=False
     )
     note = forms.CharField(
         label="Other Allergies / Notes",
         max_length=200,
-        widget=forms.TextInput(attrs={'class': css_class + " w-full"}),
+        widget=forms.TextInput(attrs={'class': css_class}),
         required=False
     )
 
