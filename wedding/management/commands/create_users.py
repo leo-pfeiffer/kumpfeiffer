fe05f21bd67d42cf99d8dc0274a50ce2b78b02b0
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-07-20 16:54:31 -0400

    feat: only create on superuser

--- wedding/management/commands/create_users.py
+++ wedding/management/commands/create_users.py
@@ -7,18 +7,6 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        USERS = [
-            "leopold",
-            "kristina",
-            "tina",
-            "paul"
-        ]
-        self.create_users(USERS)
-
-    @staticmethod
-    def create_users(users):
+        name = os.environ.get("DJANGO_SUPERUSER_NAME")
         password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-        for user in users:
-            get_user_model().objects.create_superuser(
-                user, 'admin@myproject.com', password
-            )
+        get_user_model().objects.create_superuser(name, 'admin@myproject.com', password)
