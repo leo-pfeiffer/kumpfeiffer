22958be2c63beeefa32be1373ba8b81dc9d7ba65
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-07-21 07:54:50 -0400

    feat: implement daily backup command

--- wedding/dropbox_backup.py
+++ wedding/dropbox_backup.py
@@ -1,6 +1,5 @@
 import dropbox
 import io
-import json
 from datetime import date
 
 from kumpfeiffer.settings import DROPBOX
@@ -48,25 +47,11 @@ def dropbox_upload_file(content, dropbox_file_path):
         raise e
 
 
-def data_to_binary(data):
-    return json.dumps(data).encode("utf-8")
-
-
 def backup_files(files_data: list[dict]):
 
     folder_name = f"{date.today().isoformat()}-backup"
 
     for file in files_data:
-        binary = data_to_binary(file["data"])
         dropbox_path = f"/{folder_name}/{file['name']}"
-        meta = dropbox_upload_file(binary, dropbox_path)
-        print(meta)
-
-
-# files = [
-#     {"data": {"hello": "world1"}, "name": "file1.json"},
-#     {"data": {"hello": "world2"}, "name": "file2.json"},
-#     {"data": {"hello": "world3"}, "name": "file3.json"},
-# ]
-#
-# backup_files(files)
+        meta = dropbox_upload_file(file["data"], dropbox_path)
+        print(f"Uploaded: {meta.name}")
---
+++ wedding/management/commands/daily_backup.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.core import serializers
+
+from wedding.dropbox_backup import backup_files
+from wedding.models import Rsvp, Allergy
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+
+        models = [
+            {"model": get_user_model(), "name": "users.json"},
+            {"model": Rsvp, "name": "rsvp.json"},
+            {"model": Allergy, "name": "allergy.json"},
+        ]
+
+        files_data = []
+
+        for model in models:
+            serialized = serializers.serialize("json", model["model"].objects.all())
+            files_data.append(
+                {"data": serialized.encode("utf-8"), "name": model["name"]}
+            )
+
+        backup_files(files_data)
