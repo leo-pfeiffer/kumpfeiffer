6f4be5bafd6880f971c906eb8ace7667d9de57c4
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-07-29 08:50:12 -0400

    debug: change database setup to match tutorial

--- kumpfeiffer/settings.py
+++ kumpfeiffer/settings.py
@@ -90,11 +90,21 @@ WSGI_APPLICATION = "kumpfeiffer.wsgi.application"
 #         "PORT": os.environ.get("DB_PORT"),
 #     }
 # }
-if os.getenv("DATABASE_URL", None) is None:
-    raise Exception("DATABASE_URL environment variable not defined")
-DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-}
+
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+
+elif len(sys.argv) > 0 and sys.argv[1] != "collectstatic":
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 
 # Password validation
